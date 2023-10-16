b7ca06a58e9a3a24d99a6a00400d067139eb6a07
Jhosefx <jhosefx.guilherme@gmail.com>
2023-10-16 20:27:13 -0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.menu');const navMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('Ativo');
+    NavMenu.classList.toggle('Ativo')
+
+
+
+})
